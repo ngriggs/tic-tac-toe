aad221df59c2bff9bdb8e0d4339e6a9cf0549a65
ngriggs <noahg@vt.edu>
2021-06-02 22:39:10 -0500

    added basic functionality. need to work on game state logic (i.e. who won)

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const ticTacToe = (() => {
-    const gameBoard = () => {
-        const gameboard = []
-    }
+    let turn = 'X'
+    let jimmiesMoves = []
+    let jeffsMoves = []
     return {
+        gameBoard: function() {
+            console.log(this.getAttribute('id'))
+
+            if (turn == 'X') {
+                jimmie.chooseLocation(document.getElementById(this.getAttribute('id')))
+                jimmiesMoves.push(parseInt(this.getAttribute('id')))
+            } else {
+                jeff.chooseLocation(document.getElementById(this.getAttribute('id')))
+                jeffsMoves.push(parseInt(this.getAttribute('id')))
+            }
+            console.log(jimmiesMoves)
+            console.log(jeffsMoves)
+            ticTacToe.findWinningCombinations(jimmiesMoves)
+            ticTacToe.findWinningCombinations(jeffsMoves)
+            ticTacToe.nextTurn()
+        },
         displayController: function() {
             const bodyH1Selector = document.querySelector('body').querySelector('h1')
             const container = document.createElement('div')
@@ -15,18 +31,34 @@ const ticTacToe = (() => {
                 const div = document.createElement('div');
                 div.classList.add('cell')
                 div.setAttribute('id', i)
+                div.addEventListener('click', ticTacToe.gameBoard)
                 gridDiv.appendChild(div)
             }
         gridDiv.style.gridTemplateColumns = "repeat(3, 1fr";
         gridDiv.style.gridTemplateRows = "repeat(3, 1fr";
+        },
+        nextTurn: function() {
+            turn = turn === 'X' ? 'O' : 'X';
+        },
+        findWinningCombinations: function(array) {
+            const winningCombinations = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+            const found = array.some(r=> winningCombinations.includes(r))
+            console.log(found)
         }
-
     };
 
   })();
 
 ticTacToe.displayController()
-
 
 const Player = (name, symbol) => {
     const getName  = () => name;
@@ -41,9 +73,5 @@ const Player = (name, symbol) => {
     return {name, symbol, chooseLocation}
 } 
 
-
 const jimmie = Player('Jim', 'X')
 const jeff = Player('Jeff', 'O')
-
-jimmie.chooseLocation(document.getElementById(0))
-jeff.chooseLocation(document.getElementById(1))
